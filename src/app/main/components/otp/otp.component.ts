31386f5911b0fc763924d98bdd8ad607ac850925
Melvin Masote <Melvin.Masote@gpaa.gov.za>
2020-05-16 19:39:42 +0200

    finalized otp page and registration page UI components

--- src/app/main/components/otp/otp.component.ts
+++ src/app/main/components/otp/otp.component.ts
@@ -1,4 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import {ErrorStateMatcher} from '@angular/material/core';
+import {FormControl, FormGroupDirective, NgForm, Validators} from '@angular/forms';
+
+
+/** Error when invalid control is dirty, touched, or submitted. */
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 @Component({
   selector: 'app-otp',
@@ -6,10 +17,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./otp.component.scss']
 })
 export class OtpComponent implements OnInit {
+  valid = false;
+  recaptcha: string;
 
   constructor() { }
 
   ngOnInit() {
   }
+  resolved(response: string) {
+    this.recaptcha = response;
+    if (this.recaptcha != null){
+      this.valid = true;
+    }
+    else{
+      this.valid = false;
+    }
+  }
+
+  optNum1 = new FormControl('',[
+      Validators.required
+      ]);
+  optNum2 = new FormControl('',[
+    Validators.required
+  ]);
+  optNum3 = new FormControl('',[
+    Validators.required
+  ]);
+  optNum4 = new FormControl('',[
+    Validators.required
+  ]);
+
+  matcher = new MyErrorStateMatcher();
 
 }
